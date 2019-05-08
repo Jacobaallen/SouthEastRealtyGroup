c85004d9eca5cee1301f2065352519997395784d
Jacob Allen <boat.mon@gmail.com>
2019-05-08 18:18:00 -0400

    working on fixing logo in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
+
+console.log(logo);
+
 class Header extends Component {
   render () {
     return (
       <div className="navbar">
         <div className="links">
           <ul>
-            <img className="img-responsive" src{"../../public/assets/image/img/Southeast-real-estate-logo.png" alt="logo"}/>
+            function logo() {
+              <img src{logo} alt="logo" />;
+            }
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/listings">Listings</Link>
