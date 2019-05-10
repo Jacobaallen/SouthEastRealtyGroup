a5ea22d5daff5268e3379b4f706976c8c40a0902
Jacob Allen <boat.mon@gmail.com>
2019-05-10 17:04:15 -0400

    added a hero image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import '../styles/home.css';
 
 class Home extends Component {
   render () {
     return (
       <div className="master">
+        <div className="hero-image">
+          <div className="hero-text">
+            <p>At South East Realty Group we strive for excellence.</p>
+            <p>-Steve Allen</p>
+          </div>
+        </div>
       <div className="title">
         <p> Southeast Realty Group Inc.</p>
       </div>
