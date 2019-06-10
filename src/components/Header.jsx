541a5548832b85058d2adff44994dbb16e4a21a8
Jacob Allen <boat.mon@gmail.com>
2019-06-10 16:29:10 -0400

    working on fixing a bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,35 +6,32 @@ class Header extends Component {
   render () {
     return (
 
-      <div className="navbar">
-        <div className="links">
-          <Nav activeKey="/home"onSelect={selectedKey => alert(`selected ${selectedKey}`)}>
-            <Nav.Item>
-              <Nav.Link href="/home">Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="/about">About</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="/listings">Listings</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="/contact">Contact</Nav.Link>
-            </Nav.Item>    
-        </Nav>
-
-          <ul>
-
-            <img src="/assets/image/Southeast-real-estate.png" alt="logo" />;
-
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/listings">Listings</Link>
-            <Link to="/contact">Contact</Link>
-
+      <nav className="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
+        <a className="navbar-brand" href="#home">
+          <img src="images/Southeast-real-estate.png" height="30" alt="SERSG logo"/>
+        </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="/home">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/about">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/listings">Listings</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/contact">Contact</a>
+            </li>
           </ul>
         </div>
-      </div>
+      </nav>
+
+
     )
   }
 }
