2004996039144a97531b8ad99b1ed107d8598728
Jacob Allen <boat.mon@gmail.com>
2019-07-16 19:02:46 -0400

    working on css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ class Header extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
         <a className="navbar-brand" href="#home">
-          <img src="images/Southeast-real-estate.png" height="30" alt="SERSG logo"/>
+          <img src="./public/assets/image/Southeast-real-estate.png" height="30" alt="SERSG logo"/>
         </a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
